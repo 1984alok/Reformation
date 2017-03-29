b338f169aa44af21e902ef66ea6e37988d2b91eb
1984alok <alok.oca@gmail.com>
2017-03-30 00:13:38 +0530

    changes

--- app/src/main/java/com/reformation/home/SplashActivity.java
+++ app/src/main/java/com/reformation/home/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this,AudioGuideActivity.class));
+                startActivity(new Intent(SplashActivity.this,HomeScreen.class));
             }
         },1000);
     }
